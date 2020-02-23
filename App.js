805c0d37d9500cff1532311e57352ab3207c8474
Ichrak92 <56369109+Ichrak92@users.noreply.github.com>
2020-02-23 19:47:00 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Wrapper from './Wrapper';
+
+function App() {
+  return (
+    <Wrapper>
+
+	<Photo src="/profile.png" />
+
+	<Title style={{
+
+		color: "red"
+
+}}>My Name here</Title>
+
+	<Title small>My job here</Title>
+
+
+</Wrapper>
+   
+  );
+  }
+export default App;
